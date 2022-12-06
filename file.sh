b6e848b2e12dd3f623e997b4488f13c9659aaa50
Ahmed M. A. Elsherbini <49863685+AhmedElsherbini@users.noreply.github.com>
2022-12-06 17:28:04 +0100

    Update file.sh

--- file.sh
+++ file.sh
@@ -35,16 +35,87 @@ bakta_db download --output /beegfs/work/tu_bcoea01/bakta
 
  done
 
-##############################
+###########################################################################################################################################
+#0
 #for phylogentic analysis
 #Make sure to have an outgroup species #in this anaylsis was 
 #This analysis was done on the HPC
+#Make sure that folder contains your islates only in faa file no other extra files even if in other extension
 phylophlan -i /beegfs/work/tu_bcoea01/my_micro/acco/faa_files_CA  -d phylophlan  --nproc 32 --diversity low  -f supermatrix_aa.cfg --databases_folder ./newfolder  --verbose   -o acco_mac_T_12_6
 #further annoation was done on ITOL (https://itol.embl.de/)
-##############################
+##############################################################################################################1
+#1
 #for the analysis of FastAni
 ls -1 -d *.fna > genome_list.txt
 fastANI --rl genome_list.txt --ql genome_list.txt -t 32 -o fastani_output.txt
 #Then the output you can use the R script for making graph
+#############################################################################################################
+#2
+#based on ref: https://github.com/KLemonLab/DpiMGE_Manuscript/blob/master/SupplementalMethods_Anvio.md
+#https://anvio.org/help/7/programs/anvi-setup-ncbi-cogs/
+#################################################################################################
+#mkdir -p "analysis_Anvio7/Reformat_Dpig"
+#path_f="./1_input_fna/"
+#path_o="./2_format_fna/"
+#############################################################################################################
+#for file in $path_f/*.f*; do
+  #  FILENAME=`basename ${file%.*}`
+ #   anvi-script-reformat-fasta -o $path_o/$FILENAME.fa --min-len 0 --simplify-names $file --seq-type NT; 
+#done
+
+
+#############################################################################################################
+#cd ./2_format_fna
+#############################################################################################################
+#for d in *.fa ; do f=$(echo $d | sed -E "s/\.fa*//") ; prokka --outdir $f  --prefix $f  $d ; done
+#############################################################################################################
+#mkdir -p "analysis_Anvio7/Parsed_prokka_Dpig"
+#path_f="/home/ahmed/analysis_Anvio7/2_format_fna/gff/"
+#path_o="/home/ahmed/analysis_Anvio7/3_gff_parsed"
+
+#for file in $path_f/*.gff; do
+  #  FILENAME=`basename ${file%.*}`
+ #   python gff_parser.py --gene-calls $path_o/calls_$FILENAME.txt --annotation $path_o/annot_$FILENAME.txt $file;
+#done
+
+#############################################################################################################
+#mkdir -p "analysis_Anvio7/Contigs_db_prokka_Dpig"
+#############################################################################################################
+#path_f="/home/ahmed/analysis_Anvio7/2_format_fna"
+#path_o="/home/ahmed/analysis_Anvio7/4_contigs_DB"
+#path_e="/home/ahmed/analysis_Anvio7/3_gff_parsed"
+
+#for file in $path_f/*.fa; do
+ #   FILENAME=`basename ${file%.*}`
+  #  anvi-gen-contigs-database -f $file \
+   #                           -o $path_o/$FILENAME.db \
+    #                          --external-gene-calls $path_e/calls_$FILENAME.txt \
+     #                         --ignore-internal-stop-codons \
+      #                        -n $FILENAME;
+#done
+
+#############################################################################################################
+#path_f="/home/ahmed/analysis_Anvio7/4_contigs_DB"
+#path_e="/home/ahmed/analysis_Anvio7/3_gff_parsed"
+
+#for file in $path_f/*.db; do
+   # FILENAME=`basename ${file%.*}`
+  #  anvi-import-functions -c $file \
+ #                         -i $path_e/annot_$FILENAME.txt
+      
+#done
+
+  
+
+#anvi-setup-ncbi-cogs --just-do-it
+#############################################################################################################
+#this needs the cluster
+path_f="/home/ahmed/analysis_Anvio7/4_contigs_DB"
+
+for file in $path_f/*.db; do
+    anvi-run-ncbi-cogs -T 4 --sensitive -c $file;
+done
+
+#############################################################################################################
 
 
