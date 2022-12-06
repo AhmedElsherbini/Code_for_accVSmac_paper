ca37726dc672e5e3847bab117e5205357c4cb788
Ahmed M. A. Elsherbini <49863685+AhmedElsherbini@users.noreply.github.com>
2022-12-06 14:33:48 +0100

    Update file.sh

--- file.sh
+++ file.sh
@@ -4,3 +4,4 @@
 phylophlan -i /beegfs/work/tu_bcoea01/my_micro/acco/faa_files_CA  -d phylophlan  --nproc 32 --diversity low  -f supermatrix_aa.cfg --databases_folder ./newfolder  --verbose   -o acco_mac_T_12_6
 
 #for the analysis of FastAni
+./fastANI --ql [QUERY_LIST] --rl [REFERENCE_LIST] -o [OUTPUT_FILE]
