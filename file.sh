744c9ab30a39256132c9f1f455892c81d7a87cdd
Ahmed M. A. Elsherbini <49863685+AhmedElsherbini@users.noreply.github.com>
2022-12-06 14:33:05 +0100

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,1 +1,6 @@
-In The name of Allah
+#In The name of Allah the most merciful 
+#for phylogentic analysis
+
+phylophlan -i /beegfs/work/tu_bcoea01/my_micro/acco/faa_files_CA  -d phylophlan  --nproc 32 --diversity low  -f supermatrix_aa.cfg --databases_folder ./newfolder  --verbose   -o acco_mac_T_12_6
+
+#for the analysis of FastAni
